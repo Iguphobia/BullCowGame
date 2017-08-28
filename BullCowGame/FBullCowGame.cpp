c11ae0cd3e740b120e4f0dbe5319b382e5242b0d
Igor Nogueira <igornogueira96@gmail.com>
2017-08-28 15:52:12 -0300

    Let's start using Git here.

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,93 @@
+#include "stdafx.h"
+#include "FBullCowGame.h"
+
+using FString = std::string;
+using int32 = int32;
+
+FBullCowGame::FBullCowGame()
+{
+	Reset();
+}
+
+void FBullCowGame::Reset()
+{
+	constexpr int32 MAX_TRIES = 8;
+	MyMaxTries = MAX_TRIES;
+
+	const FString HIDDEN_WORD = "planet";
+	MyHiddenWord = HIDDEN_WORD;
+	
+	MyCurrentTry = 1;
+}
+
+int32 FBullCowGame::GetMaxTries() const
+{
+	return MyMaxTries;
+}
+
+int32 FBullCowGame::GetCurrentTry() const
+{
+	return MyCurrentTry;
+}
+
+int32 FBullCowGame::GetHiddenWordLength() const
+{
+	return MyHiddenWord.length();
+}
+
+bool FBullCowGame::IsGameWon() const
+{
+	return false;
+}
+
+EGuessStatus FBullCowGame::ValidateGuess(FString CurrentGuess) const
+{
+	if (false) // if the guess isn't an isogram
+	{
+		return EGuessStatus::Not_Isogram;
+	}
+	else if (false) // if the guess isn't all lowercase
+	{
+		return EGuessStatus::Not_Lowercase;
+	}
+	else if (CurrentGuess.length() != GetHiddenWordLength()) // if the guess length is wrong
+	{
+		return EGuessStatus::Wrong_Length;
+	}
+	else // otherwise, return ok
+	{
+		return EGuessStatus::Ok;
+	}
+}
+
+// receives a valid guess, increments turn, and returns count
+FBullCowCount FBullCowGame::SubmitGuess(FString Guess)
+{
+	// increment the turn numbr
+	MyCurrentTry++;
+
+	// setup a return value
+	FBullCowCount BullCowCount;
+
+	// loop through all letters in the guess
+	int32 WordLength = MyHiddenWord.length();
+	for (int32 i = 0; i < WordLength; i++) 
+	{
+		// compare letters against hidden word;
+		for (int32 j = 0; j < WordLength; j++)
+		{
+			if (Guess[j] == MyHiddenWord[i])
+			{
+				if (i == j)
+				{
+					BullCowCount.Bulls++;
+				}
+				else
+				{
+					BullCowCount.Cows++;
+				}
+			}
+		}
+	}
+	return BullCowCount;
+}
---
+++ BullCowGame/main.cpp
@@ -0,0 +1,102 @@
+// main.cpp: Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "FBullCowGame.h"
+
+using FText = std::string;
+using int32 = int;
+
+//prototypes
+void PrintIntro();
+void PlayGame();
+FText GetValidGuess();
+bool AskToPlayAgain();
+
+FBullCowGame BCGame; // instantiante a new game
+
+// entry point of the application
+int main()
+{
+	// game's main loop
+	do
+	{
+		PrintIntro();
+		PlayGame();
+	} while (AskToPlayAgain());
+
+    return 0; // exit application
+}
+
+// introduce the game
+void PrintIntro() 
+{
+	std::cout << "Welcome to Bulls and Cows!\n";
+	std::cout << "Can you guess what " << BCGame.GetHiddenWordLength();
+	std::cout << " letter isogram I am thinking?\n";
+	std::cout << std::endl;
+}
+
+void PlayGame()
+{
+	BCGame.Reset(); // starts a new game
+	int32 MaxTries = BCGame.GetMaxTries();
+
+	// loop the number of guesses
+	for (int32 i = 1; i <= MaxTries; i++) // TODO change FOR to While when validating tries
+	{
+		FText Guess = GetValidGuess(); 
+		
+		// submit valid guess to the game and receive count
+		FBullCowCount BullCowCount = BCGame.SubmitGuess(Guess);
+
+		std::cout << "Bulls: " << BullCowCount.Bulls;
+		std::cout << ". Cows: " << BullCowCount.Cows << ".\n\n";
+	}
+
+	// TODO show game summary
+}
+
+//take a guess from the player
+FText GetValidGuess()
+{
+	EGuessStatus Status = EGuessStatus::Invalid_Status;
+	FText Guess = "";
+	
+	do 
+	{
+		int32 CurrentTry = BCGame.GetCurrentTry();
+
+		std::cout << "Try " << CurrentTry << ". Take a guess: ";
+		std::getline(std::cin, Guess);
+
+		Status = BCGame.ValidateGuess(Guess);
+
+		switch (Status)
+		{
+		case EGuessStatus::Not_Isogram:
+			std::cout << "The word must be an isogram (without repeating letters)\n";
+			break;
+		case EGuessStatus::Wrong_Length:
+			std::cout << "Please enter a " << BCGame.GetHiddenWordLength() << " letter word.\n";
+			break;
+		case EGuessStatus::Not_Lowercase:
+			std::cout << "Please insert only lowercase letters.\n";
+			break;
+		}
+		std::cout << std::endl;
+	} while (Status != EGuessStatus::Ok); // keep looping until we get no errors
+
+	return Guess;
+}
+
+// asks if the player wants to play again
+bool AskToPlayAgain()
+{
+	FText PlayAgainAns;
+	std::cout << "Do you want to play again?\n";
+	std::getline(std::cin, PlayAgainAns);
+	return (PlayAgainAns[0] == 'y') || (PlayAgainAns[0] == 'Y');
+}
